6b1d801ee1767a8de059a47bba310ec97bb4f89b
Andy Li <liandy1028@gmail.com>
2024-10-21 21:32:30 -0700

    improved update_lambda cli ui

--- lambda_functions/update_lambda_functions.py
+++ lambda_functions/update_lambda_functions.py
@@ -9,10 +9,12 @@ import base64
 
 def main():
     while True:
+        print()
         print('1. Pull remote functions')
         print('2. Push local functions')
         print('3. Exit')
         choice = input('Enter your choice: ')
+        print()
         choice, *options = choice.split()
         if choice == '1':
             if '-y' in options or '-f' in options:
@@ -46,7 +48,7 @@ def update_lambda_function(function):
         ZipFile=open(f'{function}.zip', 'rb').read(),
     )
     os.remove(f'{function}.zip')
-    print(json.dumps(response, indent=2))
+    # print(json.dumps(response, indent=2))
 
 def get_local_functions():
     return [f.name for f in (Path(__file__).parent / 'functions').iterdir()]
@@ -56,7 +58,7 @@ def fetch_lambda_functions():
     response = client.list_functions()
     return [f['FunctionName'] for f in response['Functions']]
 
-def pull_remote_function():
+def pull_remote_function(function):
     client = boto3.client('lambda', region_name = os.getenv("AWS_REGION"))
     response = client.get_function(FunctionName=function)
     with urllib.request.urlopen(response['Code']['Location']) as f:
